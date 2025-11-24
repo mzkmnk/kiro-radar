e455f480562ab482f54b2b91ba95f834f9f4f8b3
mzkmnk <sma0729mex@gmail.com>
2025-11-24 11:27:04 +0900

    style: run clippy

--- src/app.rs
+++ src/app.rs
@@ -16,7 +16,7 @@ impl App {
     pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
         self.running = true;
         while self.running {
-            terminal.draw(|frame| render(frame))?;
+            terminal.draw(render)?;
             handle_crossterm_events(&mut self)?;
         }
         Ok(())
